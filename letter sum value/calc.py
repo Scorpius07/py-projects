3a9268959200afdaa25d37a2b4b4caf50501db8a
Scorpius07 <85307012+Scorpius07@users.noreply.github.com>
2022-10-19 12:51:57 +0530

    Added the calc.py file
    Used for the calculation in the word hehe

---
+++ letter sum value/calc.py
@@ -0,0 +1,37 @@
+dict_char = {
+        '': 0,
+        'a': 1,
+        'b': 2,
+        'c': 3,
+        'd': 4,
+        'e': 5,
+        'f': 6,
+        'g': 7,
+        'h': 8,
+        'i': 9,
+        'j': 10,
+        'k': 11,
+        'l': 12,
+        'm': 13,
+        'n': 14,
+        'o': 15,
+        'p': 16,
+        'q': 17,
+        'r': 18,
+        's': 19,
+        't': 20,
+        'u': 21,
+        'v': 22,
+        'w': 23,
+        'x': 24,
+        'y': 25,
+        'z': 26
+    }
+
+
+def calculation(word):
+    total = 0
+    list_char = list(word)
+    for letter in list_char:
+        total += (dict_char.get(letter))
+    return total, word
